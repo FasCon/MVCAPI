ccbc53dcd5414080edd8079d6280521e254812fb
hhloi <h.huuloi@yahoo.com.vn>
2015-08-07 15:23:47 +0700

    add BusinessLogic

---
+++ FasCon.BusinessLogic/ProductsBL.cs
@@ -0,0 +1,35 @@
+﻿using FasCon.DataAccess.Repositories;
+using FasCon.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasCon.BusinessLogic
+{
+    public class ProductsBL
+    {
+        public static List<ProductModel> Products()
+        {
+            return ProductRepository.Products();
+        }
+        public static ProductModel FindProductById(Guid ProductId)
+        {
+            return ProductRepository.FindProductById(ProductId);
+        }
+        public static ProductModel FindProductBySEOURL(string SEOURL)
+        {
+            return ProductRepository.FindProductBySEOURL(SEOURL);
+        }
+
+        public static ProductModel FindProductByCategoryId(Guid CategoryId)
+        {
+            return ProductRepository.FindProductByCategoryId(CategoryId);
+        }
+        public static ProductModel FindProductByCategorySEOURL(string SEOURL)
+        {
+            return ProductRepository.FindProductByCategorySEOURL(SEOURL);
+        }
+    }
+}
